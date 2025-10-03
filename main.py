46a4029e4466f9a43fd2812cdbd2e24889774c86
Dharm Patel <dharmpat@outlook.com>
2025-10-02 22:54:43 -0400

    Accepts city name and returns current weather conditions

--- main.py
+++ main.py
@@ -8,14 +8,18 @@ API_KEY = os.getenv("API_KEY") # reads API_KEY variable and assigns to local API
 
 city_name = input("Enter city name: ")
 
-location_api = f"http://api.openweathermap.org/geo/1.0/direct?q={city_name}&limit=1&appid={API_KEY}"
+location_api = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={city_name}&limit=1&appid={API_KEY}")
 
-coordinates = requests.get(location_api)
-
-if coordinates.status_code == 200:
-    cord_data = coordinates.json()
+if location_api.status_code == 200:
+    cord_data = location_api.json()
     latitude = cord_data[0]["lat"]
     longitude = cord_data[0]["lon"]
 
-print(latitude, longitude)
+weather_api = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={API_KEY}")
+
+if weather_api.status_code == 200:
+    weather_data = weather_api.json()
+    weather = weather_data["weather"][0]["main"]
+
+print(weather)
 

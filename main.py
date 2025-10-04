803655a9942d42986f77e608ee6dd6ad4107131e
Dharm Patel <dharmpat@outlook.com>
2025-10-04 04:24:16 -0400

    error handling

--- main.py
+++ main.py
@@ -6,20 +6,36 @@ import os
 load_dotenv() # loads variables from .env
 API_KEY = os.getenv("API_KEY") # reads API_KEY variable and assigns to local API_KEY 
 
-city_name = input("Enter city name: ")
+def get_coords():
+    city = input("Enter city name: ").lower()
 
-location_api = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={city_name}&limit=1&appid={API_KEY}")
+    location_api = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={city}&limit=1&appid={API_KEY}")
 
-if location_api.status_code == 200:
-    cord_data = location_api.json()
-    latitude = cord_data[0]["lat"]
-    longitude = cord_data[0]["lon"]
+    if not location_api.json():
+        print("City not found, please try again!")
+        return get_coords()
+    
+    elif location_api.status_code == 200:
+        cord_data = location_api.json()
+        latitude = cord_data[0]["lat"]
+        longitude = cord_data[0]["lon"]
 
-weather_api = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={API_KEY}")
+    return latitude, longitude, city
 
-if weather_api.status_code == 200:
-    weather_data = weather_api.json()
-    weather = weather_data["weather"][0]["main"]
+def get_weather(latitude, longitude):
+    weather_api = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={API_KEY}")
 
-print(weather)
+    if weather_api.status_code == 200:
+        weather_data = weather_api.json()
+        weather = weather_data["weather"][0]["main"]
+        temp= round(int(weather_data["main"]["temp"]) - 273.15, 2) #kelvin temperature converted to celcius
+    
+    return weather, temp
 
+def main():
+    lat, lon,city = get_coords()
+    weather, temp = get_weather(lat,lon)
+    print("Weather in", city.title() + ":", weather, temp, "Celcius.")
+
+if __name__ == "__main__":
+    main()
